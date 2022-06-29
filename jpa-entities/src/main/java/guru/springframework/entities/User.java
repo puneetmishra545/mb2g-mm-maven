040326a1648ff6bc5910f8c4e91eb5a2bde19154
puneet mishra <puneetmishra545@gmail.com>
2022-06-28 23:50:00 -0400

    Created jpa-entities module

---
+++ jpa-entities/src/main/java/guru/springframework/entities/User.java
@@ -0,0 +1,24 @@
+package guru.springframework.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
